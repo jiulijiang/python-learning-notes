33e2904c1c18d70f52ef8e5fafbcd4354a5a6a85
jiulijiang <2769188733@qq.com>
2025-06-28 16:37:57 +0800

    第一次推送测试

---
+++ DAY_1/day_1.py
@@ -0,0 +1,15 @@
+#变量
+def bianliang():
+    money = 100
+    print("money = ",money) #通过print()函数输出变量
+    money = money + 100
+    print("买了100块钱的东西，现在money = ",money)
+
+
+
+
+
+
+
+# main
+bianliang()
